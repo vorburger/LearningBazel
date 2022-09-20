1deae063c7e2285c96415f132e45388eac69d480
Michael Vorburger <mike@vorburger.ch>
2022-09-20 18:55:17 +0000

    Minor changes in Java example

--- java-one/src/main/java/ch/vorburger/learningbazel/Main.java
+++ java-one/src/main/java/ch/vorburger/learningbazel/Main.java
@@ -2,6 +2,10 @@ package ch.vorburger.learniningbazel;
 
 public class Main {
     public static void main(String[] args) {
-        Syystem.out.println("hello, world");
+        System.out.println(greeting());
+    }
+
+    static String greeting() {
+        return "hello, world";
     }
 }
